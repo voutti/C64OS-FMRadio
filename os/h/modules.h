c92df60204192b34cfcb04c128487cd40d7c552b
Timo Voutilainen <voutti@users.noreply.github.com>
2026-08-11 20:14:47 +0300

    os/h/modules.h : refactored for 64tass
    Signed-off-by: Timo Voutilainen <voutti@users.noreply.github.com>

--- os/h/modules.h
+++ os/h/modules.h
@@ -22,25 +22,57 @@ syscall  .segment ;lxxx,routine
          .endm
 
 inc_h    .segment
-         .include "os/h/@1.h"
+         .include "os/h/"..\1..".h"
          .endm
 
 inc_s    .segment
-         .include "os/s/@1.s"
+         .include "os/s/"..\1..".s"
          .endm
 
 inc_t    .segment
-         .include "os/s/t/@1.s"
+         .include "os/s/t/"..\1..".s"
          .endm
 
 inc_k    .segment
-         .include "os/s/ker/@1.s"
+         .include "os/s/ker/"..\1..".s"
          .endm
 
 inc_tkh  .segment
-         .include "os/tk/h/@1.h"
+         .include "os/tk/h/"..\1..".h"
          .endm
 
 inc_tks  .segment
-         .include "os/tk/s/@1.s"
+         .include "os/tk/s/"..\1..".s"
          .endm
+
+;---------------------------------------
+;TMP-compatible versions of the include macros (original TurboMacroPro).
+;Reference only - do NOT enable these under 64tass.
+;TMP uses the @1 text parameter (which substitutes inside .include) and
+;CBM paths (leading // and a : filename separator). 64tass instead needs
+;the \1 value parameter with `..` string concat, as in the active versions
+;above (@1 does not expand inside a 64tass .include).
+;
+;inc_h    .segment
+;         .include "//os/h/:@1.h"
+;         .endm
+;
+;inc_s    .segment
+;         .include "//os/s/:@1.s"
+;         .endm
+;
+;inc_t    .segment
+;         .include "//os/s/t/:@1.s"
+;         .endm
+;
+;inc_k    .segment
+;         .include "//os/s/ker/:@1.s"
+;         .endm
+;
+;inc_tkh  .segment
+;         .include "//os/tk/h/:@1.h"
+;         .endm
+;
+;inc_tks  .segment
+;         .include "//os/tk/s/:@1.s"
+;         .endm
